6b6b18c5c252af446ec992cde525fd11b8cddec3
Muhammad Zain Ul Haq <mzuhaq7@gmail.com>
2021-11-09 18:07:04 +0500

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Home } from "./Home";
+import { Login } from "./Login";
+import { Signup } from "./Signup";
+import { Sentiment } from "./Sentiment-page";
 
-function App() {
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {/* <Login/> */}
+      {/* <Signup/> */}
+      <Home/>
+      {/* <Sentiment /> */}
+    </>
+
   );
+
 }
 
 export default App;
